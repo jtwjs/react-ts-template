fb6758df71bd476c915fed4304dd407c7c104689
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-05-19 18:12:48 +0900

    feat: add styled-components & styled-reset

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './app.css';
+import { GlobalStyle} from "./styles/global-style";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <GlobalStyle />
+    </>
   );
 }
 
